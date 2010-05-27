bfe3b8b8610458bd38a73b024b0867d54764f7e4
HarpyWar <HarpyWar@1ee03db6-bff4-d00e-2d8a-e60472eece07>
2010-05-27 03:05:17 +0000

    It's open source now

---
+++ WC3Launcher/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WC3Launcher
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.chkClearCash = new System.Windows.Forms.CheckBox();
+            this.txtGameExePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.wbNews = new System.Windows.Forms.WebBrowser();
+            this.chkLaunchGameOnStart = new System.Windows.Forms.CheckBox();
+            this.chkUseOpenGL = new System.Windows.Forms.CheckBox();
+            this.chkUseWindowMode = new System.Windows.Forms.CheckBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.linkAdv = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGo.Location = new System.Drawing.Point(353, 320);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(127, 74);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // chkClearCash
+            // 
+            this.chkClearCash.AutoSize = true;
+            this.chkClearCash.Location = new System.Drawing.Point(12, 376);
+            this.chkClearCash.Name = "chkClearCash";
+            this.chkClearCash.Size = new System.Drawing.Size(163, 17);
+            this.chkClearCash.TabIndex = 4;
+            this.chkClearCash.Text = "Очищать кеш (bncache.dat)";
+            this.chkClearCash.UseVisualStyleBackColor = true;
+            this.chkClearCash.CheckedChanged += new System.EventHandler(this.chkClearCash_CheckedChanged);
+            // 
+            // txtGameExePath
+            // 
+            this.txtGameExePath.Location = new System.Drawing.Point(12, 320);
+            this.txtGameExePath.Name = "txtGameExePath";
+            this.txtGameExePath.ReadOnly = true;
+            this.txtGameExePath.Size = new System.Drawing.Size(287, 20);
+            this.txtGameExePath.TabIndex = 5;
+            this.txtGameExePath.TextChanged += new System.EventHandler(this.txtGameExePath_TextChanged);
+            this.txtGameExePath.Click += new System.EventHandler(this.txtGameExePath_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "w3l.exe";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(305, 318);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(29, 23);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.wbNews);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(469, 263);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Новости сервера";
+            // 
+            // wbNews
+            // 
+            this.wbNews.Location = new System.Drawing.Point(6, 19);
+            this.wbNews.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbNews.Name = "wbNews";
+            this.wbNews.Size = new System.Drawing.Size(457, 238);
+            this.wbNews.TabIndex = 3;
+            // 
+            // chkLaunchGameOnStart
+            // 
+            this.chkLaunchGameOnStart.AutoSize = true;
+            this.chkLaunchGameOnStart.Location = new System.Drawing.Point(12, 353);
+            this.chkLaunchGameOnStart.Name = "chkLaunchGameOnStart";
+            this.chkLaunchGameOnStart.Size = new System.Drawing.Size(162, 17);
+            this.chkLaunchGameOnStart.TabIndex = 9;
+            this.chkLaunchGameOnStart.Text = "Запускать игру при старте";
+            this.chkLaunchGameOnStart.UseVisualStyleBackColor = true;
+            this.chkLaunchGameOnStart.CheckedChanged += new System.EventHandler(this.chkLaunchGameOnStart_CheckedChanged);
+            // 
+            // chkUseOpenGL
+            // 
+            this.chkUseOpenGL.AutoSize = true;
+            this.chkUseOpenGL.Location = new System.Drawing.Point(180, 376);
+            this.chkUseOpenGL.Name = "chkUseOpenGL";
+            this.chkUseOpenGL.Size = new System.Drawing.Size(142, 17);
+            this.chkUseOpenGL.TabIndex = 10;
+            this.chkUseOpenGL.Text = "Использовать OpenGL";
+            this.chkUseOpenGL.UseVisualStyleBackColor = true;
+            this.chkUseOpenGL.CheckedChanged += new System.EventHandler(this.chkUseOpenGL_CheckedChanged);
+            // 
+            // chkUseWindowMode
+            // 
+            this.chkUseWindowMode.AutoSize = true;
+            this.chkUseWindowMode.Location = new System.Drawing.Point(180, 354);
+            this.chkUseWindowMode.Name = "chkUseWindowMode";
+            this.chkUseWindowMode.Size = new System.Drawing.Size(140, 17);
+            this.chkUseWindowMode.TabIndex = 11;
+            this.chkUseWindowMode.Text = "Запускать игру в окне";
+            this.chkUseWindowMode.UseVisualStyleBackColor = true;
+            this.chkUseWindowMode.CheckedChanged += new System.EventHandler(this.chkUseWindowMode_CheckedChanged);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // linkAdv
+            // 
+            this.linkAdv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkAdv.Location = new System.Drawing.Point(18, 5);
+            this.linkAdv.Name = "linkAdv";
+            this.linkAdv.Size = new System.Drawing.Size(457, 31);
+            this.linkAdv.TabIndex = 7;
+            this.linkAdv.TabStop = true;
+            this.linkAdv.Text = "Рекламная ссылка";
+            this.linkAdv.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkAdv.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkAdv_LinkClicked);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 406);
+            this.Controls.Add(this.chkUseWindowMode);
+            this.Controls.Add(this.chkUseOpenGL);
+            this.Controls.Add(this.chkLaunchGameOnStart);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.linkAdv);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtGameExePath);
+            this.Controls.Add(this.chkClearCash);
+            this.Controls.Add(this.btnGo);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WarCraft III Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.CheckBox chkClearCash;
+        private System.Windows.Forms.TextBox txtGameExePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.WebBrowser wbNews;
+        private System.Windows.Forms.CheckBox chkLaunchGameOnStart;
+        private System.Windows.Forms.CheckBox chkUseOpenGL;
+        private System.Windows.Forms.CheckBox chkUseWindowMode;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.LinkLabel linkAdv;
+    }
+}
+
---
+++ WC3Launcher/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Net;
+
+
+namespace WC3Launcher
+{
+
+    public partial class frmMain : Form
+    {
+        //[DllImport("w3lh.dll", EntryPoint = "GameMain")] // 0x000014C0
+       // public static extern int GameMain(int hModule);
+
+        //[DllImport("kernel32.dll", EntryPoint = "LoadLibrary")]
+        //public static extern int LoadLibrary( [MarshalAs(UnmanagedType.LPStr)] string lpLibFileName);
+
+        private string gameExePath; // путь к файлу игры
+        private bool clearCash; // очищать кеш при старте (bncash.dat)
+        private bool launchGameOnStart; // запускать игру при старте?
+        private bool useOpenGL; // использовать OpenGL?
+        private bool useWindowMode; // запускать игру в окне?
+        
+        private string bannerURL = "http://harpywar.com/wc3launcher/banner.php";
+        private bool loadBanner = true; // you can change it to enable or disable advertise text
+        private string newsURL = "http://ya.ru";
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        // TODO: баннер, подгружающийся асинхронно с моего сайта
+        //      banner.php, с содержимым "image,url"
+        // TODO: иконка для формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wbNews.Url = new Uri(newsURL);
+            this.Icon = Properties.Resources.Warcraft_3_Reign_of_Chaos;
+            notifyIcon.Text = this.Text;
+            notifyIcon.Icon=this.Icon;
+
+            // загрузка настроек
+            gameExePath = Properties.Settings.Default.gameExePath;
+            clearCash = Properties.Settings.Default.clearCash;
+            launchGameOnStart = Properties.Settings.Default.launchGameOnStart;
+            useOpenGL = Properties.Settings.Default.useOpenGL;
+            useWindowMode = Properties.Settings.Default.useWindowMode;
+
+            txtGameExePath.Text = gameExePath;
+            chkClearCash.Checked = clearCash;
+            chkLaunchGameOnStart.Checked = launchGameOnStart;
+            chkUseOpenGL.Checked = useOpenGL;
+            chkUseWindowMode.Checked = useWindowMode;
+
+            // обнулить надпись на баннере
+            linkAdv.Text = "";
+
+            // запуск игры при старте лаунчера
+            if (launchGameOnStart)
+                btnGo_Click(null, null);
+
+            if (loadBanner)
+            {
+                groupBox1.Height = 263;
+                groupBox1.Top = 39;
+                wbNews.Height = 238;
+                LoadBanner();
+            }
+            else
+            {
+                groupBox1.Height = 290;
+                groupBox1.Top = 12;
+                wbNews.Height = 265;
+            }
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            // проверка пути
+            if (gameExePath.Length == 0 || !File.Exists(gameExePath))
+                btnBrowse_Click(null, null);
+
+
+            //int hModule = LoadLibrary(@"D:\GAMES\iCCup WarCraft III 1.24d\game.dll");
+            //GameMain(hModule);
+
+            try
+            {
+                string filename = txtGameExePath.Text; // полный путь запускаемого файла
+                string directory = Directory.GetParent(filename).FullName; // путь к директории запускаемого файла
+               
+                if (chkClearCash.Checked)
+                {
+                    // удалить кеш файл
+                    File.Delete(directory + @"\bncache.dat");
+                }
+                Process game = new Process();
+                game.StartInfo.FileName = filename;
+                game.StartInfo.WorkingDirectory = directory;
+
+                // аргументы для запуска
+                if (useWindowMode)
+                    game.StartInfo.Arguments += "-window ";
+                if (useOpenGL)
+                    game.StartInfo.Arguments += "-opengl ";
+
+                this.WindowState = FormWindowState.Minimized;
+                game.Start();
+
+                
+                
+
+            } 
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "w3l.exe | *.exe";
+            DialogResult ds = openFileDialog.ShowDialog();
+            if (ds!=DialogResult.Cancel)
+                txtGameExePath.Text = openFileDialog.FileName;
+        }
+
+        private void chkClearCash_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            clearCash = chkClearCash.Checked;
+        }
+
+        private void chkLaunchGameOnStart_CheckedChanged(object sender, EventArgs e)
+        {
+            launchGameOnStart = chkLaunchGameOnStart.Checked;
+        }
+
+        private void txtGameExePath_TextChanged(object sender, EventArgs e)
+        {
+            gameExePath = txtGameExePath.Text;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.gameExePath = gameExePath;
+            Properties.Settings.Default.clearCash = clearCash;
+            Properties.Settings.Default.launchGameOnStart = launchGameOnStart;
+            Properties.Settings.Default.useOpenGL = useOpenGL;
+            Properties.Settings.Default.useWindowMode = useWindowMode;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void chkUseWindowMode_CheckedChanged(object sender, EventArgs e)
+        {
+            useWindowMode = chkUseWindowMode.Checked;
+        }
+
+        private void chkUseOpenGL_CheckedChanged(object sender, EventArgs e)
+        {
+            useOpenGL = chkUseOpenGL.Checked;
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (this.WindowState == FormWindowState.Minimized) 
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+        }
+
+        private void txtGameExePath_Click(object sender, EventArgs e)
+        {
+            btnBrowse_Click(null,null);
+        }
+
+
+
+        private void LoadBanner()
+        {
+            // динамическая подгрузка баннера, асинхронно
+            readRemoteFile(bannerURL);
+        }
+
+        private void readRemoteFile(string address)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.Encoding = Encoding.UTF8;
+                wc.DownloadStringCompleted += new DownloadStringCompletedEventHandler(wc_DownloadStringCompleted);
+                wc.DownloadStringAsync(new Uri(address));
+
+            }
+            catch {}
+        }
+
+        private string advUrl;
+        private void wc_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                string[] data = e.Result.Split("||".ToCharArray());
+
+                linkAdv.Text = data[0];
+                advUrl = data[2];
+            }
+            catch { }
+        }
+
+        private void linkAdv_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(advUrl);
+        }
+    }
+}
